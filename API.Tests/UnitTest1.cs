763d4e9ba153d4b269a0588976e4b4d840054ac6
Jefferson Neto <jeffersonneto@live.com>
2021-01-24 13:22:01 -0300

    ...

--- API.Tests/UnitTest1.cs
+++ API.Tests/UnitTest1.cs
@@ -1,8 +1,10 @@
 using API.Controllers;
 using API.Models;
 using API.Repositories;
+using API.Uow;
 using Bogus;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Moq.AutoMock;
 using Xunit;
@@ -16,13 +18,13 @@ namespace API.Tests
         {
             //Arrange
             var mocker = new AutoMocker();
-            
+
             var expected = new Faker<Marca>("pt_BR")
                 .RuleFor(m => m.Id, f => f.Random.Short(1, 100))
                 .RuleFor(m => m.Nome, f => f.Company.CompanyName())
                 .RuleFor(m => m.Ativo, f => f.Random.Bool())
                 .RuleFor(m => m.DataCriacao, f => f.Date.Recent())
-                .Generate(5);                      
+                .Generate(5);
 
             mocker.GetMock<IRepositoryBase<Marca>>().Setup(_ => _.GetAll()).ReturnsAsync(expected);
 
@@ -36,17 +38,29 @@ namespace API.Tests
             actual.GetType().GetProperty("StatusCode").GetValue(actual).Should().BeEquivalentTo(200);
         }
 
-        //[Fact]
-        //public async void AddMarca()
-        //{
-        //    Mock<MarcaRepository> mock = new Mock<MarcaRepository>(new Mock<Data.Context>().Object);
+        [Fact]
+        public async void AddMarca()
+        {
+            //Arrange
+            var mocker = new AutoMocker();
 
-        //    mock.Setup(_ => _.Add(It.IsAny<Marca>())).Returns(Task.CompletedTask).Verifiable();
+            var marca = new Faker<Marca>("pt_BR")
+                .RuleFor(m => m.Id, f => f.Random.Short(1, 100))
+                .RuleFor(m => m.Nome, f => f.Company.CompanyName())
+                .RuleFor(m => m.Ativo, f => f.Random.Bool())
+                .RuleFor(m => m.DataCriacao, f => f.Date.Recent())
+                .Generate(1)[0];                        
 
-        //    await mock.Object.Add(new Marca());
+            var controller = mocker.CreateInstance<MarcaController>();
 
-        //    mock.Verify();
-        //}
+            //Act
+            var actual = await controller.Post(marca);
+
+            //Assert            
+            actual.GetType().GetProperty("StatusCode").GetValue(actual).Should().BeEquivalentTo(201);
+            mocker.GetMock<IRepositoryBase<Marca>>().Verify(m => m.Add(marca), Times.Once);
+            mocker.GetMock<IUnitOfWork>().Verify(u => u.Commit(), Times.Once);
+        }
 
         //[Fact]
         //public async void RemoveMarca()
@@ -72,17 +86,30 @@ namespace API.Tests
         //    mock.Verify();
         //}
 
-        //[Fact]
-        //public async void GetMarcaShouldNotBeNull()
-        //{
-        //    Mock<MarcaRepository> mock = new Mock<MarcaRepository>(new Mock<Data.Context>().Object);
+        [Fact]
+        public async void GetMarcaShouldNotBeNull()
+        {
+            //Arrange
+            var mocker = new AutoMocker();
 
-        //    mock.Setup(_ => _.GetById(It.Is<short>(_ => _ > 0))).ReturnsAsync(new Marca());
+            var expected = new Faker<Marca>("pt_BR")
+                .RuleFor(m => m.Id, f => f.Random.Short(1, 100))
+                .RuleFor(m => m.Nome, f => f.Company.CompanyName())
+                .RuleFor(m => m.Ativo, f => f.Random.Bool())
+                .RuleFor(m => m.DataCriacao, f => f.Date.Recent())
+                .Generate(1)[0];
 
-        //    var actual = await mock.Object.GetById(1);
+            mocker.GetMock<IRepositoryBase<Marca>>().Setup(m => m.GetById(It.Is<short>(_ => _ > 0))).ReturnsAsync(expected);
 
-        //    actual.Should().NotBeNull().And.BeOfType<Marca>();
-        //}
+            var controller = mocker.CreateInstance<MarcaController>();
+
+            //Act
+            var actual = await controller.Get(1);
+
+            //Assert
+            actual.GetType().GetProperty("Value").GetValue(actual).Should().BeEquivalentTo(expected);
+            actual.GetType().GetProperty("StatusCode").GetValue(actual).Should().BeEquivalentTo(200);
+        }
 
         //[Fact]
         //public async void GetMarcaShouldBeNull()
